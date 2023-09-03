45623ae0a77859bdec7827a6a8966a2f0bcf3743
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-03 14:07:54 -0400

    Hello-world

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+person1=$1
+person2=$2
+echo "Hello iam saikiran , learning the shell script"
+echo "$person1:Hi $person2 , how are you"
+echo "$person2: good morning $person1"
+echo "$person1: how are you $person2"
+echo "$person2:iam good, hoe are you $person1"
