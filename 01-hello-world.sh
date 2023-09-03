c56d25d06e0579b4d0a2601130873b0fbb92d099
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-03 13:51:49 -0400

    Hello-world

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,1 +1,6 @@
+#!bin/bash
 echo "Hello iam saikiran , learning the shell script"
+echo "ramesh:Hi suresh , how are you"
+echo "suresh: good morning ramesh"
+echo "ramesh: how are you suresh"
+echo "suresh:iam good, hoe are you ramesh"
