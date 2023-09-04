9dc119fbdf469bdb564dbef284fdb0ea816bf76d
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-04 14:05:29 -0400

    Hello-world

--- 09-loops.sh
+++ 09-loops.sh
@@ -1,5 +1,13 @@
 #!bin/bash
 
+id=$(id -u)
+
+if [ $id -ne 0 ]
+then
+    echo "you do not have the sudo access, please install with root access"
+    exit 1
+fi
+
 for i in $@
 do
     yum install $i -y
