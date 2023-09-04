7f9b89e8c39fa60a401ca3efb2993efaeceeaf5f
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-04 18:49:05 -0400

    Hello-world

--- 10-delete-logs.sh
+++ 10-delete-logs.sh
@@ -1,5 +1,14 @@
 #!bin/bash
-
+date=$(date +%F-%H-%M-%S)
+script_name=$0
+logfiles=/home/centos/shell-script-logs/$script_name-$date.log 
 files_to_be_deleted=$(find /home/centos/shell-script-logs -type f -name "*.log" -mtime +14)
 
 echo "$files_to_be_deleted"
+
+while read line
+do 
+    echo "deleting $line" >>$logfiles
+
+
+done <<<files_to_be_deleted
