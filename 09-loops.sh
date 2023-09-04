cf7f13c5cb5df36b60ddc0cfe2b63047f2b69923
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-04 17:12:35 -0400

    Hello-world

--- 09-loops.sh
+++ 09-loops.sh
@@ -3,7 +3,9 @@
 id=$(id -u)
 R="\e[31m"
 N="\e[0m"
-
+date=$(date +%F+%H+%M+%S)
+script_name=$0
+logfiles=/home/centos/shell-script-logs/$script_name-$date.log
 if [ $id -ne 0 ]
 then
     echo -e "$R ERROR: $N you do not have the sudo access, please install with root access"
@@ -15,11 +17,11 @@ fi
 
 for i in $@
 do
-    sudo yum list installed $i
+    sudo yum list installed $i>>logfiles
     if [ $? -ne 0 ]
     then
         echo "$i is not installed ,so installing it"
-        yum install $i -y
+        yum install $i -y >>$logfiles
     else
         echo "$i is already installed"
     fi
