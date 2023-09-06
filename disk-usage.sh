d6428d39697366041e342fa3eac2c7e840d557d3
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-06 08:57:46 -0400

    Disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,12 +1,16 @@
 #!bin/bash
 
-disk_usage=$( df -hT | grep -vE 'tmpfs|Filesystem' |awk -F ' ' '{print $6}' |cut -d'%' -f1 )
+disk_usage=$( df -hT | grep -vE 'tmpfs|Filesystem' )
 
 disk_threshold=1
 while IFS= read line
 do 
-    if [ $line -gt 1 ]
+    usage=$($line | awk -F ' ' '{print $1}' | cut -d'%' -f1)
+    if [ $usage -gt $disk_threshold ]
     then
-        echo "High Disk usage $line"
+        partition=$(df -hT | grep -vE 'tmpfs|Filesystem' |awk -F ' ' '{print $1}') 
+        echo "High Disk usage on $partition: $usage \n"
     fi
 done <<<$disk_usage
+
+
