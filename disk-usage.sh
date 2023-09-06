28ae81412d9495e54c4b69aa1f7a50231ff9744c
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-06 08:35:28 -0400

    Disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-disk_usage= $( df -hT | grep -vE 'tmpfs|Filesystem' |awk -F ' ' '{print $6}')
+disk_usage=$( df -hT | grep -vE 'tmpfs|Filesystem' |awk -F ' ' '{print $6}')
 
 while IFS= read line
 do 
