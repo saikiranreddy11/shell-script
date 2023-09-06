7814f88f4475d0e55f65f35682cbee4c280ae85e
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-06 08:41:43 -0400

    Disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,7 +5,7 @@ disk_usage=$( df -hT | grep -vE 'tmpfs|Filesystem' |awk -F ' ' '{print $6}' |cut
 disk_threshold=1
 while IFS= read line
 do 
-    if [$line -gt 1]
+    if [ $line -gt 1 ]
     then
         echo "High Disk usage $line"
     fi
