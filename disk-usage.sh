d85f36d5eed609bda95da2d37baaf3bb09e42bc6
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-06 09:04:56 -0400

    Disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,7 +8,7 @@ do
     usage=$(echo $line | awk -F ' ' '{print $6}' | cut -d '%' -f1)
     if [ $usage -gt $disk_threshold ]
     then
-        partition=$($line |awk -F ' ' '{print $1}') 
+        partition=$(echo $line |awk -F ' ' '{print $1}') 
         echo "High Disk usage on $partition: $usage \n"
     fi
 done <<<$disk_usage
