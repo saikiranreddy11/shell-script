a373782bd11b22d2e634dd6604a52c7f67546da7
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-09-06 09:58:39 -0400

    Disk-usage

--- mail.sh
+++ mail.sh
@@ -6,5 +6,5 @@ SUBJECT=$2
 BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
-
-echo "$BODY" | mail -s "High Disk Usage" saikiransudhireddy@gmail.com
+echo "all args: $@"
+#echo "$BODY" | mail -s "High Disk Usage" saikiransudhireddy@gmail.com
